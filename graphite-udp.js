6a9806ed64a938106a07df3b091aed2444061e84
Fernando Miçalli //fermads <falves@uolinc.com>
2015-11-11 12:49:10 -0200

    adding prefix, suffix and callback options; changing addMetric() to add() and; adding put(); new validation for name/value pair

--- graphite-udp.js
+++ graphite-udp.js
@@ -3,21 +3,37 @@ var util = require('util')
 
 function Client(options) {
 
-  var queue = {}, tid = 0, socket
+  var queue = {}, socket
 
   var defaults = {
     host: '127.0.0.1',
     port: 2003,
+    type: 'udp4',
+    prefix: '',
+    suffix: '',
     verbose: false,
     interval: 5000,
-    type: 'udp4'
+    callback: null
   }
 
   function init() {
     options = util._extend(defaults, options)
+
+    connect()
+
+    setInterval(send, options.interval)
+
+    return {
+      add: add,
+      put: put,
+      options: options
+    }
+  }
+
+  function connect() {
     socket = dgram.createSocket(options.type)
 
-    socket.on('close', function () {
+    socket.on('close', function() {
       log('UDP socket closed')
     })
 
@@ -26,67 +42,70 @@ function Client(options) {
     })
 
     log('Creating new Graphite UDP client')
-
-    return {
-      addMetric: addMetric,
-      options: options
-    }
   }
 
-  function addMetric(name, value, callback) {
-    var timestamp = String(Date.now()).substr(0, 10)
+  function put(name, value) {
+    add(name, value, true)
+  }
 
-    if(queue[name] === undefined) {
+  function add(name, value, replace) {
+    if(!name || isNaN(value))
+      return log('Skipping invalid name/value: '+ name +' '+ value)
+
+    if(options.prefix)
+      name = options.prefix +'.'+ name
+
+    if(options.suffix)
+      name = name +'.'+ options.suffix
+
+    if(queue[name] === undefined || replace)
       queue[name] = { value: value }
-    }
-    else {
+    else
       queue[name].value += value
-    }
 
-    queue[name].timestamp = timestamp
+    queue[name].timestamp = String(Date.now()).substr(0, 10)
 
     log('Adding metric to queue: '+ name +' '+ value)
-
-    if(tid === 0) {
-      tid = setTimeout(function() {
-        send(callback)
-      }, options.interval)
-    }
   }
 
-  function getQueueAsPlainText() {
+  function getQueue() {
     var text = ''
+
     for(var name in queue) {
       text += name +' '+ queue[name].value +' '+ queue[name].timestamp +'\n'
     }
+
     return text
   }
 
-  function send(callback) {
-    if(Object.keys(queue).length === 0) {
-      log('Queue is empty. Nothing to send')
-      return
-    }
+  function send() {
+    if(Object.keys(queue).length === 0)
+      return //log('Queue is empty. Nothing to send')
 
-    var metrics = new Buffer(getQueueAsPlainText())
+    var metrics = new Buffer(getQueue())
 
-    log('Sending '+ Object.keys(queue).length +' metrics to '+ options.host +':'+ options.port)
+    log('Sending '+ Object.keys(queue).length +' metrics to '
+      + options.host +':'+ options.port)
 
-    socket.send(metrics, 0, metrics.length, options.port, options.host, function(err, bytes) {
-      callback(err, bytes)
-      if(err) {
-        log('Error sending metrics: '+ err)
-      }
+    console.log(options.hos)t;
+    socket.send(metrics, 0, metrics.length, options.port, options.host,
+      function(err) {
+        console.log(err);
+      if(err)
+        return log('Error sending metrics: '+ err)
+
+      if(options.callback)
+        options.callback(err, metrics.toString())
+
+      log('Metrics sent:\n'+ metrics.toString())
     })
 
     queue = {}
-    tid = 0
   }
 
   function log(line) {
-    if(options.verbose) {
-      console.log(line)
-    }
+    if(options.verbose)
+      console.log('[graphite-udp]', line)
   }
 
   return init()
--- readme.md
+++ readme.md
@@ -2,9 +2,9 @@
 
 Graphite UDP client for Node.js
 
-* UDP for max performance
+* UDP for maximum performance
 * Uses the plaintext Graphite protocol
-* Group values for a configurable interval of time and send 1 request
+* Group values for a configurable interval and send 1 request
 * No other modules dependency
 
 
@@ -15,32 +15,84 @@ npm install graphite-udp
 
 ## Usage
 ```js
-graphiteUdp = require('./graphite-udp').createClient([options])
-graphiteUdp.addMetric(metric, value, [callback])
+var graphite = require('graphite-udp')
+
+var metric = graphite.createClient([options])
+metric.add(name, value)
+metric.put(name, value)
 ```
 
 `options` is an object with the following defaults:
 ```js
 {
-	host: '127.0.0.1',
-	port: 2003,
-	verbose: false,
-	interval: 5000, // group metrics for 5s and send only 1 request
-	type: 'udp4'
-}
-```
-
-## Example
-```js
-graphiteUdp = require('./graphite-udp').createClient({verbose:true});
-for (var i = 0; i < 100; i++) {
-	graphiteUdp.addMetric('my.test.metric', parseInt(Math.random()*100, 10));
+  host: '127.0.0.1', // graphite server host
+  port: 2003, // graphite server default udp port
+  type: 'udp4', // udp type (udp4 or udp6)
+  prefix: '', // a prefix to prepend to the name of all matrics
+  suffix: '', // a suffix to append to the name of all metrics
+  verbose: false, // log messages to console
+  interval: 5000, // group metrics for 5s and send only 1 request
+  callback: null // callback(error, metricsSent) for when metrics are sent to server
 }
 ```
 
 Make sure your Carbon is listening for UDP connections on carbon.conf
 
 `ENABLE_UDP_LISTENER = True # default for version 0.9.10 of Graphite is False`
+
+
+## Example
+```js
+var os = require('os')
+var graphite = require('graphite-udp')
+var metric = graphite.createClient({
+  prefix: 'myproductname',
+  suffix: os.hostname(),
+  interval: 60000,
+  verbose: true,
+  callback: function(error, metricsSent) {
+    console.log('Metrics sent\n'+ metricsSent)
+  }
+})
+
+metric.add('my.test.metric1', 10) // add
+metric.add('my.test.metric1', 20) // add 10+20
+metric.put('my.test.metric2', 1) // put
+metric.put('my.test.metric2', 5) // put overwrite 1 with 5
+
+```
+
+Will generate
+```
+myproductname.my.test.metric1.NI-67936-0 30 1447193969
+myproductname.my.test.metric2.NI-67936-0 5 1447193969
+```
+
+# API
+
+### metric.add
+During the `interval` time option, if 2 or more metrics with the same name
+are sent, metrics will be added (summed)
+```js
+metrics.add('my.test.metric', 20)
+metrics.add('my.test.metric', 10)
+```
+What will actually be sent to the server after `interval` is
+```
+my.test.metric 30
+```
+
+### metric.put
+During the `interval` time option, if 2 or more metrics with the same name
+are sent, the last one will be used
+```js
+metrics.put('my.test.metric', 20)
+metrics.put('my.test.metric', 10)
+```
+What will actually be sent to the server after `interval` is
+```
+my.test.metric 10
+```
 
 
 ## License
